7b91b56cc393f2eb3f6f85f63648d35b4746d607
NEhnes <162652120+NEhnes@users.noreply.github.com>
2025-06-19 17:22:38 -0400

    sort alg work

--- 2dFightTesting/WinScreen.cs
+++ 2dFightTesting/WinScreen.cs
@@ -115,6 +115,21 @@ namespace _2dFightTesting
         private void SortPlayersList()
         {
             // DO LATER
+
+            int temp;
+            for (int i = 0; i < size - 1; i++)
+            {
+                for (int j = 0; j < size - i - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+
         }
         private void OverwriteXml()
         {
